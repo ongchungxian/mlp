78ab7f2532d8e0e49332af4f0e29db112011b3b0
Rayhana <s1610748@sms.ed.ac.uk>
2022-03-11 18:45:46 +0000

    Pre-processed data and set up data_model object

---
+++ data_model.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+class data_model:
+    def __init__(self,metadatapath='data\\metadata.fea',timeseriespath='data\\timeseries.fea'):
+        self.metadata = pd.read_feather(metadatapath)
+        self.timeseries = pd.read_feather(timeseriespath)
+    
+    def datetime(self):
+        return self.timeseries['datetime']
+
+    def panels(self):
+        return [int(x) for x in self.timeseries.columns[1:].tolist()]
+    
+    def index(self,i):
+        i = int(i)
+        row_metadata = self.metadata.loc[self.metadata['ss_id'] == i]
+        row_series = self.timeseries[str(i)]
+        return (row_metadata, row_series)
+    
+    
--- tmp.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-f1 = pd.read_csv('system_metadata.csv')
-f2 = pd.read_csv('system_metadata_location_rounded.csv')
-
-print(f1)
-print(f2)
