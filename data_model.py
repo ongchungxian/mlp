674e06aac82b07c86020f0b2ea1fe4d2b0be281b
Rayhana <s1610748@sms.ed.ac.uk>
2022-03-29 14:25:19 +0100

    Update data_model.py

--- data_model.py
+++ data_model.py
@@ -39,6 +39,26 @@ class data_model:
                 y.append(self.timeseries.iloc[[full_offset+length]][station].iat[0])
         return (x_metadata,x_timeseries, y)
 
+    def test(self,its=1,length=64, pred_length=1, max_its=18):#generate a test dataset of the given length
+        if(its>max_its):
+            its=max_its
+        x_metadata = []
+        x_timeseries = []
+        y = []
+        for i in range(its):
+            for j in range(85):
+                x_metadata.append(self.metadata1.iloc[[j]][['ss_id','latitude_rounded','longitude_rounded','llsoacd','orientation','tilt','kwp']])
+                station = str(self.metadata1.iloc[[j]]['ss_id'].iat[0])
+                offset1 = 85*(length+pred_length)*i
+                offset2 = j*(length+pred_length)
+                full_offset = offset1+offset2
+                time_chunk = []
+                for k in range(length):
+                    curr_index = full_offset+k
+                    time_chunk.append(self.timeseries.iloc[[curr_index]][station].iat[0])
+                x_timeseries.append(time_chunk)
+                y.append(self.timeseries.iloc[[full_offset+length]][station].iat[0])
+        return (x_metadata,x_timeseries, y)
 
 
 
