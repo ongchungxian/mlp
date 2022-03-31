7d2c3ff8537b3ce4c5fec5312acff2f5ec2f9c16
Rayhana <s1610748@sms.ed.ac.uk>
2022-03-31 17:39:09 +0100

    Update data_model.py

--- data_model.py
+++ data_model.py
@@ -44,10 +44,13 @@ class data_model:
         y = []
         for i in range(its):
             for j in range(85):
-                x_metadata.append(self.metadata1.iloc[[j]][['ss_id','latitude_rounded','longitude_rounded','llsoacd','orientation','tilt','kwp']])
                 station = str(self.metadata1.iloc[[j]]['ss_id'].iat[0])
                 full_offset = (length+pred_length)*i
                 time_chunk = []
+                metadatum1 = self.metadata1.iloc[[j]][['ss_id','latitude_rounded','longitude_rounded','llsoacd','orientation','tilt','kwp']]
+                metadatum2 = self.timeseries.iloc[[full_offset]]['datetime']
+                metadatum = pd.concat([metadatum1, metadatum2], axis=1)
+                x_metadata.append(metadatum)
                 for k in range(length):
                     curr_index = full_offset+k
                     time_chunk.append(self.timeseries.iloc[[curr_index]][station].iat[0])
